ca4efb7d394d274b8cf982664e399b5e97f822b3
chetna1100 <chetna1100@gmail.com>
2022-07-23 15:19:59 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const input =document.getElementById('button'); 
+
 function addingEventListener() {
+    alert('I was clicked!');
 }
+input.addEventListener('click', addingEventListener)
+
+/////////call Back Function//////////
+// input.addEventListener('click', function(){
+//     alert('I was clicked!');
+// })
+
